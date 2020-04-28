abc41a26506a4bed80fd54b03fe5b45dc409c24a
Warren Longmire <warrenlongmire@gmail.com>
2020-04-28 09:21:39 -0400

    New Setup

---
+++ src/org/launchcode/techjobs/console/TechJobsComplete.java
@@ -0,0 +1,124 @@
+package org.launchcode.techjobs.console;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by LaunchCode
+ */
+public class TechJobsComplete {
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main (String[] args) {
+
+        // Initialize our field map with key/name pairs
+        HashMap<String, String> columnChoices = new HashMap<>();
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+
+        // Top-level menu options
+        HashMap<String, String> actionChoices = new HashMap<>();
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+
+        System.out.println("Welcome to LaunchCode's TechJobs App!");
+
+        // Allow the user to search until they manually quit
+        while (true) {
+
+            String actionChoice = getUserSelection("View jobs by:", actionChoices);
+
+            if (actionChoice.equals("list")) {
+
+                String columnChoice = getUserSelection("List", columnChoices);
+
+                if (columnChoice.equals("all")) {
+                    printJobs(JobDataComplete.findAll());
+                } else {
+
+                    ArrayList<String> results = JobDataComplete.findAll(columnChoice);
+
+                    System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
+
+                    // Print list of skills, employers, etc
+                    for (String item : results) {
+                        System.out.println(item);
+                    }
+                }
+
+            } else { // choice is "search"
+
+                // How does the user want to search (e.g. by skill or employer)
+                String searchField = getUserSelection("Search by:", columnChoices);
+
+                // What is their search term?
+                System.out.println("\nSearch term: ");
+                String searchTerm = in.nextLine();
+
+                if (searchField.equals("all")) {
+                    ArrayList<HashMap<String, String>> jobs = JobDataComplete.findByValue(searchTerm);
+                    printJobs(jobs);
+                } else {
+                    printJobs(JobDataComplete.findByColumnAndValue(searchField, searchTerm));
+                }
+            }
+        }
+    }
+
+    // ﻿Returns the key of the selected item from the choices Dictionary
+    private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
+
+        Integer choiceIdx;
+        Boolean validChoice = false;
+        String[] choiceKeys = new String[choices.size()];
+
+        // Put the choices in an ordered structure so we can
+        // associate an integer with each one
+        Integer i = 0;
+        for (String choiceKey : choices.keySet()) {
+            choiceKeys[i] = choiceKey;
+            i++;
+        }
+
+        do {
+
+            System.out.println("\n" + menuHeader);
+
+            // Print available choices
+            for (Integer j = 0; j < choiceKeys.length; j++) {
+                System.out.println("" + j + " - " + choices.get(choiceKeys[j]));
+            }
+
+            choiceIdx = in.nextInt();
+            in.nextLine();
+
+            // Validate user's input
+            if (choiceIdx < 0 || choiceIdx >= choiceKeys.length) {
+                System.out.println("Invalid choice. Try again.");
+            } else {
+                validChoice = true;
+            }
+
+        } while(!validChoice);
+
+        return choiceKeys[choiceIdx];
+    }
+
+    // Print a list of jobs
+    private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+
+        for (int i =0; i< someJobs.size(); i++){
+            System.out.println("****************");
+            System.out.println("position type: " + someJobs.get(i).get("position type"));
+            System.out.println("name: " + someJobs.get(i).get("name"));
+            System.out.println("employer: " + someJobs.get(i).get("employer"));
+            System.out.println("location: " + someJobs.get(i).get("location"));
+            System.out.println("core competency: " + someJobs.get(i).get("core competency"));
+        }
+    }
+}
